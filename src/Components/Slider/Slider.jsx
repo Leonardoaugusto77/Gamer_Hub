cfd2dd28df80e5d709748bcc13cc170fcd18bc4f
Heisen <leonardopjh30@gmail.com>
2023-07-17 12:40:33 -0300

    feat(slider): created model slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,5 +1,11 @@
-import { Wrapper } from "../../Styles/Wrapper";
+import { Slider_Section, Wrapper } from "../../Styles/Wrapper";
 
 export default function Slider() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Slider_Section>
+        <img src="" />
+      </Slider_Section>
+    </Wrapper>
+  );
 }
--- src/Styles/Wrapper.jsx
+++ src/Styles/Wrapper.jsx
@@ -27,3 +27,16 @@ export const Titles_Form_container = styled.div`
 export const Form_container_input = styled.div`
   width: 475px;
 `;
+
+export const Slider_Section = styled(Wrapper)`
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 500px;
+    height: 500px;
+    filter: drop-shadow(7px 7px 7px rgba(0, 0, 0, 0.25));
+    background-color: aliceblue;
+  }
+`;
